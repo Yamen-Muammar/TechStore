04e8c1135c93c1900f83308d5e4fd4eede9decb4
Yamen Muammar <yamenmuamar@gmail.com>
2026-01-31 21:54:00 +0200

    Create the Permission Interface

---
+++ TechStoreDataTier/Interfaces/IPermissionRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechStoreApp_Core_.Models;
+namespace TechStoreDataTier.Interfaces
+{
+    internal interface IPermissionRepository
+    {
+        Permission GetPermissionById(int id);
+        List<Permission> GetAllPermissions();
+        Permission GetPermissionByCode(string name);
+
+        bool AddPermission(Permission permission);
+        Permission UpdatePermissionTitle(Permission permission);
+
+    }
+}
